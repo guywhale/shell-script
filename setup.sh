434ecdce7319055b179e8db29884279eadeb9633
guywhale <43272320+guywhale@users.noreply.github.com>
2023-11-24 16:25:52 +0000

    populate SCSS file with experiment code

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-mkdir $2
-mkdir $2/dev
-touch $2/dev/variation-1.scss
-touch $2/variation-1.css
-touch $2/variation-1.js
+# Variables
+experiment_code="$2"
+hypenated_experiment_code="${experiment_code//\./-}"
+
+# Build files and directories
+mkdir $experiment_code $experiment_code/dev
+touch $experiment_code/dev/variation-1.scss $experiment_code/variation-1.css $experiment_code/variation-1.js
+
+# Populate SCSS file
+cat <<EOF > $experiment_code/dev/variation-1.scss
+\$tag: '.cv-$hypenated_experiment_code';
+
+#{\$tag} {
+
+}
+EOF
