18a240e24c7007949ce7cb0003e898f66f0243b0
guywhale <43272320+guywhale@users.noreply.github.com>
2023-11-28 12:48:14 +0000

    add Tampermonkey config file generation

--- conv.sh
+++ conv.sh
@@ -8,10 +8,10 @@ lowercase_company_code=$(echo "$company_code" | tr '[:upper:]' '[:lower:]')
 # If directories don't exist, make them
 if [ ! -d "./$experiment_number" ]; then
   mkdir $experiment_number
+fi
 
-  if [ ! -d "./$experiment_number/dev" ]; then
-    mkdir $experiment_number/dev
-  fi
+if [ ! -d "./$experiment_number/dev" ]; then
+  mkdir $experiment_number/dev
 fi
 
 # Create and populate variation-1.css
@@ -22,7 +22,7 @@ fi
 # Create and populate variation-1.js
 if [ ! -e "$experiment_number/variation-1.js" ]; then
   touch $experiment_number/variation-1.js
-  cat <<EOF > $experiment_number/variation-1.js
+  cat << EOF > $experiment_number/variation-1.js
 ((w) => {
   const tag = 'cv-$lowercase_company_code-$hypenated_experiment_number';
   const exp = '$company_code $experiment_number';
@@ -72,12 +72,31 @@ fi
 # Create and populate /dev/variation-1.scss
 if [ ! -e "$experiment_number/dev/variation-1.scss" ]; then
   touch $experiment_number/dev/variation-1.scss
-  cat <<EOF > $experiment_number/dev/variation-1.scss
+  cat << EOF > $experiment_number/dev/variation-1.scss
 \$tag: '.cv-$lowercase_company_code-$hypenated_experiment_number';
 
 #{\$tag} {
     background-color: yellow !important;
 }
+EOF
+fi
+
+# Create and populate /dev/tampermonkey.js
+if [ ! -e "$experiment_number/dev/tampermonkey.js" ]; then
+  touch $experiment_number/dev/tampermonkey.js
+  cat << EOF > $experiment_number/dev/tampermonkey.js
+// ==UserScript==
+// @name         $company_code $experiment_number - Variation 1
+// @author       Guy Whale
+// @match        http://*/*
+// @resource     cssFile file:///Users/guywhale/repos/experiments/$company_code/$experiment_number/variation-1.css]
+// @require      file:///Users/guywhale/repos/experiments/$company_code/$experiment_number/variation-1.js]
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// ==/UserScript==
+
+var cssFile = GM_getResourceText("cssFile");
+GM_addStyle(cssFile);
 EOF
 fi
 
