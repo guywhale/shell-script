47c964392398586681fec7c0a3c81e27f57f92a2
guywhale <43272320+guywhale@users.noreply.github.com>
2023-11-29 15:58:49 +0000

    restructure directories and update code to accommodate to make it easier to have experiments with more than one variant

--- conv.sh
+++ conv.sh
@@ -5,9 +5,6 @@ client_code_lowercase=$(echo "$client_code" | tr '[:upper:]' '[:lower:]')
 experiment_number="$2"
 experiment_number_hyphenated="${experiment_number//\./-}"
 
-# Parameter string for replacing placeholders in templates with variables
-replace_placeholders="s/<<<CLIENT_CODE>>>/$client_code/g; s/<<<CLIENT_CODE_LOWERCASE>>>/$client_code_lowercase/g; s/<<<EXPERIMENT_NUMBER>>>/$experiment_number/g; s/<<<EXPERIMENT_NUMBER_HYPHENATED>>>/$experiment_number_hyphenated/g"
-
 # Locations of default templates
 templates_path="$HOME/bin/templates"
 js_template="$templates_path/default/JS.js"
@@ -36,10 +33,25 @@ if [ ! -d "./$experiment_number/dev" ]; then
   mkdir $experiment_number/dev
 fi
 
+sass_path="$experiment_number/dev/scss"
+
+if [ ! -d "./$sass_path" ]; then
+  mkdir $sass_path
+fi
+
+tm_path="$experiment_number/dev/tm"
+
+if [ ! -d "./$tm_path" ]; then
+  mkdir $tm_path
+fi
+
 # Create and populate variation-1.css
 if [ ! -e "$experiment_number/variation-1.css" ]; then
   touch $experiment_number/variation-1.css
 fi
+
+# Parameter string for replacing placeholders in templates with variables
+replace_placeholders="s/<<<CLIENT_CODE>>>/$client_code/g; s/<<<CLIENT_CODE_LOWERCASE>>>/$client_code_lowercase/g; s/<<<EXPERIMENT_NUMBER>>>/$experiment_number/g; s/<<<EXPERIMENT_NUMBER_HYPHENATED>>>/$experiment_number_hyphenated/g"
 
 # Create and populate variation-1.js
 if [ ! -e "$experiment_number/variation-1.js" ]; then
@@ -47,27 +59,38 @@ if [ ! -e "$experiment_number/variation-1.js" ]; then
   cat "$js_template" | sed "$replace_placeholders" >> $experiment_number/variation-1.js 
 fi
 
-# Create and populate /dev/variation-1.scss
-if [ ! -e "$experiment_number/dev/variation-1.scss" ]; then
-  touch $experiment_number/dev/variation-1.scss
-  cat "$sass_template" | sed "$replace_placeholders" >> $experiment_number/dev/variation-1.scss
+# Create and populate /dev/scss/variation-1.scss
+if [ ! -e "$experiment_number/dev/scss/variation-1.scss" ]; then
+  touch $experiment_number/dev/scss/variation-1.scss
+  cat "$sass_template" | sed "$replace_placeholders" >> $experiment_number/dev/scss/variation-1.scss
 fi
 
-# Create and populate /dev/tampermonkey.js
-if [ ! -e "$experiment_number/dev/tampermonkey.js" ]; then
-  touch $experiment_number/dev/tampermonkey.js
-  cat "$tm_template" | sed "$replace_placeholders" >> $experiment_number/dev/tampermonkey.js 
+# Create and populate /dev/tm/tm-variation-1.js
+if [ ! -e "$experiment_number/dev/tm/tm-variation-1.js" ]; then
+  touch $experiment_number/dev/tm/tm-variation-1.js
+  cat "$tm_template" | sed "$replace_placeholders" >> $experiment_number/dev/tm/tm-variation-1.js 
 fi
+
+conv_id="\n[CONV] $client_code $experiment_number ->"
 
 # Check if sass CLI is installed globally and install if not
 if command -v sass &> /dev/null; then
-  echo "\n[CONV] "$client_code $experiment_number" -> Ready\n"
+  echo "$conv_id Ready\n"
 else
-  echo "[CONV] "$client_code $experiment_number" -> Installing Sass globally via NPM..."
+  echo "$conv_id Installing SASS globally via NPM..."
   npm install -g sass
+  sleep 1
+
+  if command -v sass &> /dev/null; then
+    echo "$conv_id SASS successfully installed.\n"
+    sleep 1
+    echo "$conv_id Ready\n"
+  else
+    echo "$conv_id SASS installation failed. Please install SASS globally. Exiting\n"
+    exit 1
+  fi
 fi
 
-cd $experiment_number/dev
-
 # Run SASS compiler
-sass --watch --no-source-map variation-1.scss ../variation-1.css
+# cd $experiment_number/dev
+sass --watch --no-source-map ./$sass_path/variation-1.scss ./$experiment_number/variation-1.css
