42c521b053add1d6a5d698f91cf3bc2eec5ef598
guywhale <43272320+guywhale@users.noreply.github.com>
2023-12-12 11:11:09 +0000

    remove DEMO templates as not needed in personal branch

--- templates/DEMO/V1.js
+++
@@ -1,5 +0,0 @@
-// DEMO Javascript template
-console.log('<<<CLIENT_CODE>>>');
-console.log('<<<CLIENT_CODE_LOWERCASE>>>');
-console.log('<<<EXPERIMENT_NUMBER>>>');
-console.log('<<<EXPERIMENT_NUMBER_HYPHENATED>>>');
